f565ddc7826da8e63655d4d3c1901ce90ab29318
Josue Garcia <yoshuah809@gmail.com>
2022-01-17 12:59:20 -0600

    app initialization

---
+++ src/components/Home.vue
@@ -0,0 +1,40 @@
+<template>
+	<b-container>
+		<b-row>
+			<b-col>
+				<h1>Welcome to this course</h1>
+			</b-col>
+		</b-row>
+		<b-row>
+			<b-col v-for="net in network" :key="net.net" class="text-center">
+				<b-link :href="net.url" target="_blank">
+					<b-alert :variant="net.color" show> {{ net.net }}</b-alert>
+				</b-link>
+			</b-col>
+		</b-row>
+	</b-container>
+</template>
+
+<script>
+export default {
+	name: "Home",
+	data() {
+		return {
+			network: [
+				{
+					net: "Facebook",
+					url: "https://facebook.com",
+					color: "info",
+				},
+				{
+					net: "tweeter",
+					url: "https://tweeter.com",
+					color: "danger",
+				},
+			],
+		};
+	},
+};
+</script>
+
+<style lang="stylus" scoped></style>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+//installing bootstrap dependencies
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
